06d3825d5c2fd6827c44c54f195c68a4dc8787a6
Antoine <arocq92@gmail.com>
2025-10-17 16:54:14 +0200

    fix Repository - adaptation aux changes de Entity Controllers BDD - test not done yet

--- src/Repository/EmpruntRepository.php
+++ src/Repository/EmpruntRepository.php
@@ -6,15 +6,13 @@ use App\Entity\Emprunt;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Emprunt>
- */
 class EmpruntRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Emprunt::class);
     }
+
     public function countActiveEmpruntsByUser(int $utilisateurId): int
     {
         return $this->createQueryBuilder('e')
@@ -36,28 +34,4 @@ class EmpruntRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-    //    /**
-    //     * @return Emprunt[] Returns an array of Emprunt objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('e.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Emprunt
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
--- src/Repository/LivreRepository.php
+++ src/Repository/LivreRepository.php
@@ -6,52 +6,23 @@ use App\Entity\Livre;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Livre>
- */
 class LivreRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Livre::class);
     }
+
     public function findBooksOfAuthorBorrowedBetweenDates(int $auteurId, \DateTimeInterface $startDate, \DateTimeInterface $endDate): array
-{
-    return $this->createQueryBuilder('l')
-        ->innerJoin('l.emprunts', 'e')
-        ->innerJoin('l.id_auteur', 'a')
-        ->where('a.id = :auteurId')
-        ->andWhere('e.dateEmprunt BETWEEN :startDate AND :endDate')
-        ->setParameter('auteurId', $auteurId)
-        ->setParameter('startDate', $startDate)
-        ->setParameter('endDate', $endDate)
-        ->getQuery()
-        ->getResult();
-}
-
-
-    //    /**
-    //     * @return Livre[] Returns an array of Livre objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('l')
-    //            ->andWhere('l.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('l.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Livre
-    //    {
-    //        return $this->createQueryBuilder('l')
-    //            ->andWhere('l.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    {
+        return $this->createQueryBuilder('l')
+            ->innerJoin('l.emprunts', 'e')
+            ->where('l.auteur = :auteurId')
+            ->andWhere('e.dateEmprunt BETWEEN :startDate AND :endDate')
+            ->setParameter('auteurId', $auteurId)
+            ->setParameter('startDate', $startDate)
+            ->setParameter('endDate', $endDate)
+            ->getQuery()
+            ->getResult();
+    }
 }
